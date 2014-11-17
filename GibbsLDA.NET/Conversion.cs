4a12f066d7c871eefb768d819a2d08b35ffde135
BAmine <amine.benhalloum@gmail.com>
2014-11-17 16:51:33 +0100

    Word and ID dictionaries

---
+++ GibbsLDA.NET/Conversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GibbsLDA.NET
+{
+    public class Conversion
+    {
+        public static string ZeroPad(int number, int width)
+        {
+            var result = new StringBuilder();
+            for (int i = 0; i < number.ToString().Length; i++)
+            {
+                result.Append("0");
+            }
+            result.Append(number.ToString());
+            return result.ToString();
+        }
+    }
+}
