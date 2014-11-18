99092e41fc4f3ec8c2864ac0f225f35a4bfb320c
BAmine <amine.benhalloum@gmail.com>
2014-11-18 12:48:44 +0100

    some constants

---
+++ GibbsLDA.NET/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GibbsLDA.NET
+{
+    public class Constants
+    {
+        public static readonly int MODEL_STATUS_UNKNOWN = 0;
+        public static readonly int MODEL_STATUS_EST = 1;
+        public static readonly int MODEL_STATUS_ESTC = 2;
+        public static readonly int MODEL_STATUS_INF = 3;
+    }
+}
